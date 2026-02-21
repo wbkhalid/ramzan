52ba06a5a891c60b8c01b4a6efb1e41f9d82a15a
wbkhalid <waleedbinkhalidch@gmail.com>
2026-02-21 15:25:57 +0500

    remove /

--- app/APIs.ts
+++ app/APIs.ts
@@ -10,7 +10,7 @@ export const SAHULAT_BAZAR_DASHBOARD_API = "/api/Dashboard";
 export const STALL_API = "/api/Stalls";
 export const COMMODITY_API = "/api/Commodities";
 export const CHECK_LIST_API = "/api/CheckLists";
-export const Daily_Monitoring_API = "/api/DastarkhawanDailyMonitoring/";
+export const Daily_Monitoring_API = "/api/DastarkhawanDailyMonitoring";
 
 // NEXTJS ROUTES API
 export const NEXTJS_AUTH_API = "/api/my-auth";
